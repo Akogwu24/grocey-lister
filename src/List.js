9d6f70d01f8c49d9f2fe5be3582c4780c6f52512
Akogwu24 <akogwu.emmanuel24@gmail.com>
2021-08-02 06:18:59 +0100

    local storage added

--- src/List.js
+++ src/List.js
@@ -1,7 +1,35 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
-}
+import React from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
-export default List
+const List = ({ items, removeItem, editItem }) => {
+  return (
+    <div className='grocery-list'>
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <div className='grocery-item' key={id}>
+            <p className='title'>{title}</p>
+            <div className='btn-container'>
+              <button
+                onClick={() => editItem(id)}
+                type='button'
+                className='edit-btn'
+              >
+                <FaEdit />
+              </button>
+              <button
+                onClick={() => removeItem(id)}
+                type='button'
+                className='delete-btn'
+              >
+                <FaTrash />
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default List;
